0b16fb30296297d01aecbe4fd80b44740e950ef7
Marc Donatiello <marc.donatiello@gmail.com>
2019-09-23 18:53:52 -0400

    Committing the latest code and images.

---
+++ Materialize js library/parallax-template/js/init_murali.js
@@ -0,0 +1,19 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.materialboxed');
+  var instances = M.Materialbox.init(elems, options);
+});
+
+
+var instance = M.Materialbox.getInstance(elem);
+
+instance.open();
+instance.close();
